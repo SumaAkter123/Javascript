0ef9681de4eae718d5c521c3029e2e5c02a26fd4
Suma Akter.mst <136326044+SumaAkter123@users.noreply.github.com>
2023-09-02 17:55:26 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,41 @@
+
+//text-change
+function textChange(){
+    document.getElementById('h1').innerHTML="Hello world";
+}
+//text-hi
+function textHi(){
+    document.getElementById('h2').innerHTML="Hello world";
+}
+//text-list
+function textList(){
+    document.getElementById('h3').innerHTML="Hello world";
+}
+//text-open
+function textOpen(){
+    document.getElementById('h4').innerHTML="Hello world";
+}
+//text-about
+function textAbout(){
+    document.getElementById('h5').innerHTML="Hello world";
+}
+//text-hello
+function textHello(){
+    document.getElementById('h6').innerHTML="Hello world";
+}
+//text-from
+function textFrom(){
+    document.getElementById('h7').innerHTML="Hello world";
+}
+//text-active
+function textActive(){
+    document.getElementById('h8').innerHTML="Hello world";
+}
+//text-hide
+function textHide(){
+    document.getElementById('h9').innerHTML="Hello world";
+}
+//text-format
+function textFormat(){
+    document.getElementById('h10').innerHTML="Hello world";
+}
